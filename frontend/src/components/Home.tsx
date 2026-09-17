12e3f4b2514573f3db8b4487ffa3fa110dd4f470
Brito-Reji <britoreji03@gmail.com>
2026-09-17 23:22:48 +0530

    refactor: remove unused Link import from Home component

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,5 +1,5 @@
 import { useState, type FormEvent } from 'react'
-import { useNavigate, Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { BACKEND_URL } from '../utils/config'
 import logger from '../utils/logger'
 
